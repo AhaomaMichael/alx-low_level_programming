c7368e2a70103a3027b323b1763852adf437f32b
AhaomaMichael <ahaomamichael@gmail.com>
2022-06-08 20:08:01 -0700

    update2_5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,14 +7,14 @@
  *         0 if the number is zero,
  *         -1 if the number is less than zero.
  */
-int print_sign(int a)
+int print_sign(int n)
 {
-	if (a > 0)
+	if (n > 0)
 	{
 		_putchar ('+');
 		return (1);
 	}
-	else if (a == 0)
+	else if (n == 0)
 	{
 		_putchar ('0');
 		return (0);
